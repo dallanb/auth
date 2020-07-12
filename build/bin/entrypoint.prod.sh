e9bc18d5a58b1b5611dd5e48e3637ec6eb4c6e92
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-07-11 20:29:51 -0700

    trim the fat

--- build/bin/entrypoint.prod.sh
+++ build/bin/entrypoint.prod.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 gunicorn --bind 0.0.0.0:5000 manage:app
--- build/bin/entrypoint.sh
+++ build/bin/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 . ~/.bashrc
 
 manage run -h 0.0.0.0
