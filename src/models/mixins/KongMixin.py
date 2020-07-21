be4c868b2ead17a5bd06a45ccaf004c5adaf18b0
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-07-21 10:50:55 -0700

    rename directory to better identify its purpose: proxy -> external. the change is for external apis

--- src/models/mixins/KongMixin.py
+++ src/models/mixins/KongMixin.py
@@ -1,20 +1,20 @@
 import base64, json, pickle
 from sqlalchemy.event import listen
-from ...proxy import KongProxy
+from ...external import Kong
 
 
 class KongMixin(object):
     @staticmethod
     def create_consumer(uuid, username):
-        return KongProxy().create_consumer(uuid=str(uuid), username=username)
+        return Kong().create_consumer(uuid=str(uuid), username=username)
 
     @staticmethod
     def create_jwt_credential(username, key):
-        return KongProxy().create_jwt_credential(username=username, key=key, algorithm='HS256')
+        return Kong().create_jwt_credential(username=username, key=key, algorithm='HS256')
 
     @staticmethod
     def destroy_jwt_credential(username, jwt_id):
-        return KongProxy().delete_jwt_credential(username=username, jwt_id=jwt_id)
+        return Kong().delete_jwt_credential(username=username, jwt_id=jwt_id)
 
     @classmethod
     def after_insert(cls, mapper, connection, target):
