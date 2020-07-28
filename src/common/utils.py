41dee129b2dfdda964330634f6055da89c1a15db
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-07-27 18:32:51 -0700

    remove junk that no longer applies to the service

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,14 +1,6 @@
 import json
 
 
-def create_message(message):
-    return {'type': 'success', 'message': message}
-
-
-def create_error(message):
-    return {'type': 'danger', 'message': message}
-
-
 def get_json(myjson):
     try:
         json_object = json.loads(myjson)
