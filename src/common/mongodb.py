72a21380ae8ce008f7a31f22f563ee2fdd43669e
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-08-21 15:18:57 -0700

    add a common mongo client

--- src/common/mongodb.py
+++ src/common/mongodb.py
@@ -1,8 +1,33 @@
-from ..common.error import *
-from ..common.cleaner import Cleaner
-from .. import mongodb
+import collections
 
 
 class MongoDB:
     def __init__(self):
         pass
+
+    @classmethod
+    # Methods
+    def init(cls, model, **kwargs):
+        return model(**kwargs)
+
+    @classmethod
+    def count(cls, model):
+        return model.objects.count()
+
+    @classmethod
+    def save(cls, instance):
+        instance.save()
+        return instance
+
+    @classmethod
+    # TODO: Consider using dataclass instead of a named tuple
+    def find(cls, model, **kwargs):
+        items = model.objects(**kwargs)
+        total = items.count()
+        Find = collections.namedtuple('Find', ['items', 'total'])
+        return Find(items=items, total=total)
+
+    @classmethod
+    def destroy(cls, instance):
+        instance.delete()
+        return True
