499e4b0dcfd87e52e8307d66ad909881db316c6f
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-08-25 23:07:33 -0700

    temporary test for ping event

--- src/routes/v1/ping.py
+++ src/routes/v1/ping.py
@@ -1,13 +1,15 @@
 from flask_restful import marshal_with
 from ...common import MessageResponse
-
+from ...services import User
 from . import Base
 
 
 class Ping(Base):
     def __init__(self):
         Base.__init__(self)
+        self.user = User()
 
     @marshal_with(MessageResponse.marshallable())
     def get(self):
+        self.user.notify(topic='auth', value={'message': 'incoming ping'}, key='user_ping')
         return MessageResponse(message='pong')
