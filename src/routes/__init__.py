a842b0cfb7e2a9104f77e77a10397bfe50224964
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-09-02 14:56:38 -0700

    add refresh route

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,8 +1,9 @@
+from .v1 import Login, Logout, Ping, Register, Status, Refresh
 from .. import api
-from .v1 import Login, Logout, Ping, Register, Status
 
 api.add_resource(Login, '/login', methods=['POST'])
 api.add_resource(Logout, '/logout', methods=['POST'])
 api.add_resource(Ping, '/ping', methods=['GET'])
 api.add_resource(Register, '/register', methods=['POST'])
 api.add_resource(Status, '/status', methods=['GET'])
+api.add_resource(Refresh, '/refresh', methods=['GET'])
--- src/routes/v1/__init__.py
+++ src/routes/v1/__init__.py
@@ -2,7 +2,7 @@ from .base import Base
 from .login import Login
 from .logout import Logout
 from .ping import Ping
+from .refresh import Refresh
 from .register import Register
-from .status import Status
-
 from .schemas import *
+from .status import Status
