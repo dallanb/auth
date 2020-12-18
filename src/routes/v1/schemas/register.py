3e84594c2ccf1806f8f202074780ef7743dfa1ae
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-12-18 11:07:54 -0800

    add more useful fields to the user creation

--- src/routes/v1/schemas/register.py
+++ src/routes/v1/schemas/register.py
@@ -5,8 +5,8 @@ class RegisterFormSchema(Schema):
     email = fields.Str(required=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True)
-    first_name = fields.Str(required=True)
-    last_name = fields.Str(required=True)
+    display_name = fields.Str(required=True)
+    country = fields.Str(required=True)
 
 
 register_form_schema = RegisterFormSchema()
