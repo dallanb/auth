3fdce63efe4601b2fac713fa3e1e26c0a8bb1fda
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-22 12:58:15 -0800

    add invites routes

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,4 +1,4 @@
-from .v1 import Login, Logout, Ping, Register, Status, Refresh
+from .v1 import Login, Logout, Ping, Register, Status, Refresh, Invite
 from .. import api
 
 api.add_resource(Login, '/login', methods=['POST'])
@@ -7,3 +7,4 @@ api.add_resource(Ping, '/ping', methods=['GET'])
 api.add_resource(Register, '/register', methods=['POST'])
 api.add_resource(Status, '/status', methods=['GET'])
 api.add_resource(Refresh, '/refresh', methods=['GET'])
+api.add_resource(Invite, '/invites/token', methods=['GET'])
