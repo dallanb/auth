e90188a0cf55dc15cc80f11116cfe46a7e8b0add
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-22 16:20:48 -0800

    reset ping

--- src/routes/v1/ping.py
+++ src/routes/v1/ping.py
@@ -1,6 +1,6 @@
 from flask_restful import marshal_with
 from ...common import MessageResponse
-from ...services import User, InviteToken
+from ...services import User
 from . import Base
 
 
@@ -11,5 +11,4 @@ class Ping(Base):
 
     @marshal_with(MessageResponse.marshallable())
     def get(self):
-        InviteToken().deactivate_tokens(email='dallanbhatti+1@gmail.com')
         return MessageResponse(message='pong')
