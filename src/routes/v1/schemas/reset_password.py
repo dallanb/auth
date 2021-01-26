fef9173af9da69929551a6e45fabbf83d31a5c19
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-25 17:34:35 -0800

    dumb mistake by asking password to be an email address

--- src/routes/v1/schemas/reset_password.py
+++ src/routes/v1/schemas/reset_password.py
@@ -4,7 +4,7 @@ from marshmallow.validate import Length
 
 class ResetPasswordSchema(Schema):
     token = fields.Str(required=True, validate=Length(equal=64))
-    password = fields.Email(required=True)
+    password = fields.Str(required=True)
 
 
 reset_password_schema = ResetPasswordSchema()
