851e0e79798869ec567dc7a400c836992d4bf1bd
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-19 10:09:13 -0800

    updating the sdk to be more like the rest of the existing services so that i can use the sdk to update the remaining services

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# For use by jenkins test step
+echo "Waiting for server..."
 
-. ~/.bashrc
-manage delete_db
-manage flush_cache
-manage init
-python -m py.test
+while [[ ! "$(docker ps -aq -f status=running -f health=healthy -f name=app)" ]]; do sleep 1; done
+
+echo "Server ready"
+
+docker exec app py.test --disable-pytest-warnings -s --junitxml tests.xml --cov-report xml --cov-report term --cov-branch --cov=src
