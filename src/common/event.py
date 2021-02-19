3b799b3abda69677723e5e295b51d4634c7cc945
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-19 11:05:12 -0800

    remove all the usages of classmethod

--- src/common/event.py
+++ src/common/event.py
@@ -5,11 +5,11 @@ class Event:
     def __init__(self):
         pass
 
-    @classmethod
-    def generate_endpoint(cls, topic, value):
+    @staticmethod
+    def generate_endpoint(topic, value):
         return f"/{topic}/{str(value)}"
 
-    @classmethod
-    def send(cls, topic, value, key):
+    @staticmethod
+    def send(topic, value, key):
         producer = Producer(topic=topic, value=value, key=key)
         producer.start()
