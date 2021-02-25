09a2d2d980d5908c448bb972a563002a12fdcb5b
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-24 18:10:46 -0800

    update decorators

--- src/decorators/invite_token_notification.py
+++ src/decorators/invite_token_notification.py
@@ -1,16 +1,16 @@
 from functools import wraps
 
+from src import services
+from src.notifications import invite_created
+
 
 class invite_token_notification:
     def __init__(self, operation):
         self.operation = operation
-        self.topic = 'auth'
-        self._service = None
 
     def __call__(self, f):
         @wraps(f)
         def wrap(*args, **kwargs):
-            self.service = args[0]
             new_instance = f(*args, **kwargs)
             self.create(new_instance=new_instance)
             return new_instance
@@ -19,20 +19,7 @@ class invite_token_notification:
         wrap.__name__ = f.__name__
         return wrap
 
-    @property
-    def service(self):
-        return self._service
-
-    @service.setter
-    def service(self, service):
-        self._service = service
-
-    def create(self, new_instance):
-        key = 'invite_created'
-        value = {
-            'token': str(new_instance.token),
-            'email': new_instance.email,
-            'uuid': str(new_instance.uuid),
-        }
-        self.service.notify(topic=self.topic, value=value, key=key, )
-        self.service.send_invite(instance=new_instance)
+    @staticmethod
+    def create(new_instance):
+        invite_created.from_data(invite_token=new_instance).notify()
+        services.InviteToken().send_invite(instance=new_instance)
--- src/decorators/user_notification.py
+++ src/decorators/user_notification.py
@@ -1,16 +1,15 @@
 from functools import wraps
 
+from src.notifications import auth_created, auth_updated
+
 
 class user_notification:
     def __init__(self, operation):
         self.operation = operation
-        self.topic = 'auth'
-        self._service = None
 
     def __call__(self, f):
         @wraps(f)
         def wrap(*args, **kwargs):
-            self.service = args[0]
             display_name = kwargs.pop('display_name', None)
             country = kwargs.pop('country', None)
             new_instance = f(*args, **kwargs)
@@ -24,32 +23,10 @@ class user_notification:
         wrap.__name__ = f.__name__
         return wrap
 
-    @property
-    def service(self):
-        return self._service
+    @staticmethod
+    def create(new_instance, display_name, country):
+        auth_created.from_data(user=new_instance, display_name=display_name, country=country).notify()
 
-    @service.setter
-    def service(self, service):
-        self._service = service
-
-    def create(self, new_instance, display_name, country):
-        key = 'auth_created'
-        value = {
-            'username': new_instance.username,
-            'email': new_instance.email,
-            'uuid': str(new_instance.uuid),
-            'display_name': display_name,
-            'country': country,
-            'status': new_instance.status.name
-        }
-        self.service.notify(topic=self.topic, value=value, key=key, )
-
-    def update(self, new_instance):
-        key = 'auth_updated'
-        value = {
-            'username': new_instance.username,
-            'email': new_instance.email,
-            'uuid': str(new_instance.uuid),
-            'status': new_instance.status.name
-        }
-        self.service.notify(topic=self.topic, value=value, key=key, )
+    @staticmethod
+    def update(new_instance):
+        auth_updated.from_data(user=new_instance).notify()
