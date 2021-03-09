4745679b88c1b30a1c9d9d3b2c0bd29bcc220c01
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-03-09 15:51:41 -0800

    add an update method to the services base

--- src/services/base.py
+++ src/services/base.py
@@ -1,6 +1,6 @@
 import logging
 from http import HTTPStatus
-from sqlalchemy.exc import DataError, IntegrityError, StatementError
+from sqlalchemy.exc import DataError, IntegrityError, StatementError, InvalidRequestError
 
 from ..common import Cache, DB, Event
 from ..common.error import ManualException
@@ -52,6 +52,17 @@ class Base:
             self.error(code=HTTPStatus.INTERNAL_SERVER_ERROR)
         except KeyError:
             self.logger.error(f'add error - KeyError')
+            self.db.rollback()
+            self.error(code=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+    def _update(self, query, **kwargs):
+        try:
+            update = query.update({**kwargs})
+            self._commit()
+            return update
+        except InvalidRequestError as ex:
+            self.logger.error(f'update error - InvalidRequestError')
+            self.logger.error(ex)
             self.db.rollback()
             self.error(code=HTTPStatus.INTERNAL_SERVER_ERROR)
 
