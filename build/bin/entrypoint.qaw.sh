219f57ac9155b525c5d41510920ac028639c7070
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-14 13:38:02 -0700

    fix the check for migrations:

--- build/bin/entrypoint.qaw.sh
+++ build/bin/entrypoint.qaw.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 . ~/.bashrc
+
 pip install -e .
 
 if [ "$DATABASE" = "auth" ]; then
   echo "Waiting for auth..."
-
   while ! nc -z $SQL_HOST $SQL_PORT; do
     sleep 0.1
   done
@@ -13,19 +13,26 @@ if [ "$DATABASE" = "auth" ]; then
   echo "PostgreSQL started"
 fi
 
+
 if [ ! -d "migrations/versions" ]; then
   echo "Directory migrations/versions does not exist."
-  flask db init --directory=migrations
-  sed -i '/import sqlalchemy as sa/a import sqlalchemy_utils' migrations/script.py.mako
-  flask db migrate --directory=migrations
-  sed -i 's/PasswordType(length=1137)/PasswordType(max_length=1137)/' migrations/versions/*.py
-  flask db upgrade --directory=migrations
-  manage init
-  manage load
+  init=$(flask db init --directory=migrations)
+  case $init in
+    *"Error: Directory migrations already exists and is not empty"*)
+      echo "Migrations handled elsewhere"
+      ;;
+    *)
+      sed -i '/import sqlalchemy as sa/a import sqlalchemy_utils' migrations/script.py.mako
+      flask db migrate --directory=migrations
+      sed -i 's/PasswordType(length=1137)/PasswordType(max_length=1137)/' migrations/versions/*.py
+      flask db upgrade --directory=migrations
+      manage init
+      manage load
+      ;;
+  esac
 else
   flask db migrate --directory=migrations
   flask db upgrade --directory=migrations
 fi
-
 
 gunicorn --bind 0.0.0.0:5000 manage:app
--- build/bin/entrypoint.sh
+++ build/bin/entrypoint.sh
@@ -16,13 +16,20 @@ fi
 
 if [ ! -d "migrations/versions" ]; then
   echo "Directory migrations/versions does not exist."
-  flask db init --directory=migrations
-  sed -i '/import sqlalchemy as sa/a import sqlalchemy_utils' migrations/script.py.mako
-  flask db migrate --directory=migrations
-  sed -i 's/PasswordType(length=1137)/PasswordType(max_length=1137)/' migrations/versions/*.py
-  flask db upgrade --directory=migrations
-  manage init
-  manage load
+  init=$(flask db init --directory=migrations)
+  case $init in
+    *"Error: Directory migrations already exists and is not empty"*)
+      echo "Migrations handled elsewhere"
+      ;;
+    *)
+      sed -i '/import sqlalchemy as sa/a import sqlalchemy_utils' migrations/script.py.mako
+      flask db migrate --directory=migrations
+      sed -i 's/PasswordType(length=1137)/PasswordType(max_length=1137)/' migrations/versions/*.py
+      flask db upgrade --directory=migrations
+      manage init
+      manage load
+      ;;
+  esac
 else
   flask db migrate --directory=migrations
   flask db upgrade --directory=migrations
